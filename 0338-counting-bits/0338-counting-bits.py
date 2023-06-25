7bf74e55d6a9217ca8d65ab008011937003c66f6
AMIT RANA <69069897+HUMBLEF0OL@users.noreply.github.com>
2023-06-25 11:34:18 +0530

    Time: 91 ms (81.96%), Space: 23.2 MB (5.92%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,9 +1,12 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
+        # declaring a list of size n+1
         result = [0]*(n+1)
         for i in range(n+1):
+            # if the number is even then number of ones will be equal to the number of ones at i//2 index
             if i%2 == 0:
                 result[i] = result[i//2]
+            # for odd the number of ones will be equal to number of ones at i//2 index plus 1
             else:
                 result[i] = result[i//2]+1
         return result
