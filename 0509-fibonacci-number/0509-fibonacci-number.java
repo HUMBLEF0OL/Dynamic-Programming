f35f1f2f16e44f83c7ce35b23571194aa71c0175
AMIT RANA <69069897+HUMBLEF0OL@users.noreply.github.com>
2023-06-25 14:12:56 +0530

    Time: 0 ms (100.00%), Space: 39 MB (97.13%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -21,13 +21,29 @@ class Solution {
         return dp[n];
         
     }
+    public int optimizedTabulation(int n,int dp[]){
+        // base case:
+        if(n <= 1)return n;
+        // known solution the subproblems
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for(int i =2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
     public int fib(int n) {
         // bruteforce recursive solution
         // int result = unoptimizedRecursion(n);
         
         // optimized recursion using memoization and dynamic programming
         int dp[] = new int[n+1];
-        int result = optimizedRecursion(n,dp);
+        // int result = optimizedRecursion(n,dp);
+        
+        // optimization using tabulation and dynamic programming
+        int result = optimizedTabulation(n,dp);
+        
         
         
         return result;
