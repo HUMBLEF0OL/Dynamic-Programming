9c4bc3d0cd70451cf8b8eac7d287f33e135461fd
AMIT RANA <69069897+HUMBLEF0OL@users.noreply.github.com>
2023-06-26 08:23:20 +0530

    Time: 6 ms (7.49%), Space: 40.4 MB (5.25%) - LeetHub

---
+++ 1025-divisor-game/1025-divisor-game.java
@@ -0,0 +1,24 @@
+class Solution {
+    private HashMap<Integer, Boolean> memo;
+    public boolean unoptimizedRecursion(int n, boolean turn){
+        // base case: when the value of of n is 1 then the current player will loose the game
+        if(n == 1) return false;
+        
+        if(memo.containsKey(n))return memo.get(n);
+        
+        // iterating over all possible factors of n
+        for(int i =1;i<n;i++){
+            if(n % i == 0 && unoptimizedRecursion(n-i,!turn) == false){
+                memo.put(n,true);
+                return true;
+            }
+        }
+        memo.put(n,false);
+        return false;
+    }
+    public boolean divisorGame(int n) {
+        // 0 -> Alice and 1 -> Bob
+        memo = new HashMap<>();
+        return unoptimizedRecursion(n,true);
+    }
+}
