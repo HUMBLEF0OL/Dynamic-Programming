e8fac0ba14f3d16332df0165aa9e8d0f49feac7a
AMIT RANA <69069897+HUMBLEF0OL@users.noreply.github.com>
2023-06-29 18:53:40 +0530

    Time: 6 ms (7.64%), Space: 39.7 MB (14.86%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.java
+++ 1025-divisor-game/1025-divisor-game.java
@@ -1,32 +1,35 @@
+// tricky solution
 // class Solution {
-//     private HashMap<Integer, Boolean> memo;
-//     public boolean unoptimizedRecursion(int n, boolean turn){
-//         // base case: when the value of of n is 1 then the current player will loose the game
-//         if(n == 1) return false;
-        
-//         if(memo.containsKey(n))return memo.get(n);
-        
-//         // iterating over all possible factors of n
-//         for(int i =1;i<n;i++){
-//             if(n % i == 0 && unoptimizedRecursion(n-i,!turn) == false){
-//                 memo.put(n,true);
-//                 return true;
-//             }
-//         }
-//         memo.put(n,false);
-//         return false;
-//     }
 //     public boolean divisorGame(int n) {
-//         // 0 -> Alice and 1 -> Bob
-//         memo = new HashMap<>();
-//         return unoptimizedRecursion(n,true);
-//     }
+//        if(n%2==0)
+//            return true;
+//         return false;
+//     }       
 // }
 
+
 class Solution {
-    public boolean divisorGame(int n) {
-       if(n%2==0)
-           return true;
+    private HashMap<Integer, Boolean> memo;
+    public boolean optimizedRecursion(int n, boolean turn){
+        //base case: when the value of n is 1 then the current player will loose the game
+        if(n == 1)return false;
+        
+        if(memo.containsKey(n))return memo.get(n);
+        
+        
+        // iterating over all possible factors of n
+        for(int i=1;i<n;i++){
+            if(n%i == 0 && optimizedRecursion(n-1,!turn) == false){
+                memo.put(n,true);
+                return true;
+            }
+        }
+        memo.put(n,false);
         return false;
-    }       
+    }
+    public boolean divisorGame(int n) {
+        // 0 -> Alice and 1 -> Bob
+        memo = new HashMap<>();
+        return optimizedRecursion(n,true);
+    }
 }
