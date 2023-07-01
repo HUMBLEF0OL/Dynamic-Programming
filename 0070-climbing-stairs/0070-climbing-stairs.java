56366ca3ab8c129e3e8adf5f1bf75931de181085
AMIT RANA <69069897+HUMBLEF0OL@users.noreply.github.com>
2023-07-01 16:49:02 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (37.98%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,27 @@
+class Solution {
+    // unoptimized recursive solution
+    public int unoptimizedRecursion(int n){
+        // base condition
+        if(n <= 1) return 1;
+        
+        // now find number of ways of climbing to top
+        
+        return unoptimizedRecursion(n-1) + unoptimizedRecursion(n-2);
+        
+    }
+    public int optimizedRecursion(int n,int[] dp){
+        // if we already know the solution return the answer
+        if(dp[n] != 0)return dp[n];
+        
+        // if the answer of current n is unknown then first calculate the answer then return
+        dp[n] = optimizedRecursion(n-1,dp) + optimizedRecursion(n-2,dp);
+        return dp[n];
+    }
+    public int climbStairs(int n) {
+        // return unoptimizedRecursion(n);
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        dp[1]=1;
+        return optimizedRecursion(n,dp);
+    }
+}
